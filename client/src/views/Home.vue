4b237b981849f27f5ddcd39e8aa8de2b2753f1a9
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-10-29 07:48:30 -0600

    Home content

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
-	<div class="home">
-		<header>
-			<h1>Nathaniel Wheeler</h1>
-			<h4>Full-Stack Web Developer</h4>
+	<div class="home container">
+		<header class="row">
+			<h1 class="col-12">Nathaniel Wheeler</h1>
+			<h4 class="col-12">Full-Stack Web Developer</h4>
 		</header>
-		<main>
-			<p class="lead">
+		<main class="row">
+			<p class="lead col-12 col-md-6 order-2 order-md-1">
 				I am a knowledge sponge who isn’t content unless I am learning or puzzling over something.
 				<br />Before I was a developer, I was an art student, a geology major, a calculus tutor, a bookkeeper, and even a coroner’s assistant.
 				<br />When I’m not working on projects or looking at documentation, I like to make comics, read some good ol’ scifi/fantasy, and partake in outdoor activities.
 				<br />I live in Boise, Idaho with my wife and our two cats.
 			</p>
+			<img
+				src="https://i.ibb.co/3zwVGbC/pgskxDT.jpg"
+				class="card-img-top col-12 col-md-6 order-1 order-md-2"
+				alt="Picture of Nathaniel Wheeler manning his trusty laptop."
+			/>
 		</main>
 	</div>
 </template>
