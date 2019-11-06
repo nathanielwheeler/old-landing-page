49080756bb111a40887fee29cbcd170984a80c3c
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-11-06 14:10:24 -0700

    Revert "initial store configuration"
    This reverts commit decd890b92fa78775d076b1ea5d6ca9d29750815.

--- src/store.js
+++ src/store.js
@@ -1,41 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Axios from 'axios'
-import router from './router'
-
 
 Vue.use(Vuex)
 
-//Allows axios to work locally or live
-let base = window.location.host.includes('localhost:8080') ? '//localhost:3000/' : '/'
-
-let api = Axios.create({
-	baseURL: base + "api/",
-	timeout: 3000,
-	withCredentials: true
-})
-
 export default new Vuex.Store({
-	state: {
-		latestPosts: {},
-	},
-	mutations: {
-		setLatestPosts(state, posts) {
-			state.latestPosts = posts
-		},
-	},
-	actions: {
-		// #region POSTS
+  state: {
 
-		async getLatestPosts({ commit, dispatch }) {
-			try {
-				let res = await api.get(`posts`)
-				commit('setLatestPosts', res.data)
-			} catch (error) {
-				console.error(error)
-			}
-		},
+  },
+  mutations: {
 
-		// #endregion
-	}
+  },
+  actions: {
+
+  }
 })
